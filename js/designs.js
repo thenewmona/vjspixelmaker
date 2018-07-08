eec4ccf56130963e5b73d16a2ff194fc5e22c54d
thenewmona <thenewmona@gmail.com>
2018-07-07 23:44:16 -0400

    add all the JS,  tested still does not work

--- js/designs.js
+++ js/designs.js
@@ -1,6 +1,11 @@
 // Select color input
-// Select size input
+const color = document.getElementById('#colorPicker');
 
+// Select size input
+const height = document.querySelector('#inputHeight');
+const width = document.querySelector('#inputWidth');
+const sizePicker = document.querySelector('#sizePicker');
+const height = document.querySelector('#inputHeight');
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
