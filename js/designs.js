3fe564041cf8b4e4d8f1245f0df1e2c502dee76a
thenewmona <thenewmona@gmail.com>
2018-07-12 00:13:33 -0400

    added color size input started grid. will not pass

--- js/designs.js
+++ js/designs.js
@@ -5,9 +5,42 @@ const color = document.getElementById('#colorPicker');
 const height = document.querySelector('#inputHeight');
 const width = document.querySelector('#inputWidth');
 const sizePicker = document.querySelector('#sizePicker');
-const height = document.querySelector('#inputHeight');
+const canvas = document.querySelector('#pixelCanvas');
 // When size is submitted by the user, call makeGrid()
+function clearOldGrid() {
+  //clear the grid
+  canvas.innerHTML = '';
+}
 
 function makeGrid() {
+  let tr, td;
+
   // Your code goes here!
+
+  // create table for loops 
+
+  for (let i = 0; i < height.val; i++) {
+    tr = document.createElement('tr');
+    canvas.appendChild(tr);
+    for (let j = 0; j < width.value; j++) {
+      td = document.createElement('td');
+      tr.appendChild(td);
+    }
+  }
 }
+
+function changeCellColor() {
+
+  canvas.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (e.target.nedeName === 'TD') {
+      e.target.style.bacgroundColor = color.value;
+    }
+  })
+}
+sizePicker.addEventListener('submit', function (e) {
+  e.preventDefault();
+  clearOldGrid();
+  makeGrid();
+  changeCellColor();
+})
