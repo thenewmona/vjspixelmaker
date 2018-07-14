8e74e57dbffa43b0bfe4b4c4a6eea09318862c97
thenewmona <thenewmona@gmail.com>
2018-07-14 19:30:23 -0400

    final got the grid to work, added the css

--- js/designs.js
+++ js/designs.js
@@ -1,29 +1,23 @@
 // Select color input
-let color = document.querySelector('#colorPicker');
-
+const color = document.querySelector('#colorPicker');
 // Select size input
+const height = document.querySelector('#inputHeight');
+const width = document.querySelector('#inputWidth');
+const sizePicker = document.querySelector('#sizePicker');
+const canvas = document.querySelector('#pixelCanvas');
 
-let height = document.querySelector('#inputHeight');
-let width = document.querySelector('inputWidth');
-let sizePicker = document.querySelector('sizePicker');
-let canvas = document.querySelector('pixelCanvas');
 
-//this makes it so the grid will stay, instead of disappear
-//https://www.w3schools.com/jsref/event_preventdefault.asp
-function makeGrid() {
-  //clear the old grid
+function clearOldGrid() {
+  // clear the old grid first
   canvas.innerHTML = '';
+}
+
+function makeGrid() {
   let tr, td;
-
-  // Your code goes here!
-  //make a grid
-
-  // create table with for loops
-
+  //create table: (for loop runs from inside, then outside). create td, append to tr; create tr, tr to table(canvas).
   for (let i = 0; i < height.value; i++) {
     tr = document.createElement('tr');
     canvas.appendChild(tr);
-
     for (let j = 0; j < width.value; j++) {
       td = document.createElement('td');
       tr.appendChild(td);
@@ -31,13 +25,21 @@ function makeGrid() {
   }
 }
 
-canvas.addEventListener('click', function (e) {
-  e.preventDefault();
-  if (e.target.nodeName === 'TD') {
-    e.target.style.backgroundColor = color.value;
-  }
-})
+function changeCellColor() {
+  //check which TD is clicked, change its color (Udacity Lesson 21: 5. Avoid Too Many Events talks about this)
+  canvas.addEventListener('click', function (e) {
+    e.preventDefault();
+    // check using capital letters
+    if (e.target.nodeName === 'TD') {
+      e.target.style.backgroundColor = color.value;
+    }
+  })
+}
+
+// When size is submitted by the user, call makeGrid()
 sizePicker.addEventListener('submit', function (e) {
   e.preventDefault();
+  clearOldGrid();
   makeGrid();
+  changeCellColor();
 })
